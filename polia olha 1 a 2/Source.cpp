279455d455fa9ccfd10906b4285d7c61dc80862b
MichalRusnak0 <90607780+MichalRusnak0@users.noreply.github.com>
2021-12-09 10:13:14 +0100

    Create Source.cpp

---
+++ polia olha 1 a 2/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*
+	string jedlo[5] = { "hruska","jablko","zemiak","mrkva","ananas" };
+	for (int a = 4; a >= 0; a--) {
+	
+			cout << jedlo[a] << endl;
+			if (a <= 0) {
+				return 0;
+			 }
+	}
+	*/
+
+	int cisielka[10] = { -8000, -500 ,200, 500 ,20, 50, -6000, 1100, 800, 10000 };
+	
+	
+	for (int b = 0; b <= 10000; b ++) {
+		int cisielko = b;
+
+	}
+	cout << "Najvacie cisielko je" << cisielko << endl;
+	return 0;
+
+
+}
